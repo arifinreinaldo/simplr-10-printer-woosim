7f585c5f2ec241a3db911212a862ffb98635db61
Reinaldo-GK <arifin.reinaldo@gmail.com>
2026-05-06 15:15:16 +0800

    reverse the yellow label

--- app/src/main/java/net/simplr/woosimdp230l/MainPresenter.java
+++ app/src/main/java/net/simplr/woosimdp230l/MainPresenter.java
@@ -454,7 +454,7 @@ public class MainPresenter {
     // ZPL Template Constants
     private static final String ZPL_INIT = "CT~~CD,~CC^~CT~";
     private static final String ZPL_START = "^XA~TA000~JSN^LT0^MNY^MTD^POI^PMN^LH0,0^PR3,3~SD15^LRN^CI0";
-    private static final String ZPL_START_YELLOW = "^XA~TA000~JSN^LT0^MNY^MTD^PON^PMN^LH0,0^PR3,3~SD15^LRN^CI0";
+    private static final String ZPL_START_YELLOW = "^XA~TA000~JSN^LT0^MNY^MTD^POI^PMN^LH0,0^PR3,3~SD15^LRN^CI0";
     private static final String ZPL_END = "^XZ";
 
     public void printZPL(String[] paramList, String macAddress, String printerName) {
